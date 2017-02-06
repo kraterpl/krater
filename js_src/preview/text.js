4fd3c77a187f38253591a9cc3d8d067d4281f474
kraterpl <kontakt@krater.pl>
2017-02-06 11:33:56 +0100

    bugfix: user krater.preview.text also for text posts without text body

--- js_src/preview/text.js
+++ js_src/preview/text.js
@@ -2,15 +2,16 @@ krater.preview.register({
     priority: 0,
     id: 'text',
     isApplicable: function (data) {
-        return !!data.text;
+        return !data.link;
     },
     getThumbnail: function (data) {
         return 'comments-o';
     },
     getIcon: function (data) {
-        return 'file-text-o';
+        return data.text ? 'file-text-o' : null;
     },
     getHTML: function (data) {
         return krater.util.md(data.text);
     }
 });
+
